9a770dd2e123619a85028d72892c3df0f9a52ad2
Amankumar-033 <amanbkp9135@gmail.com>
2025-07-14 14:04:22 +0530

    Debug minor issue

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer'>
       <img onClick={() => navigate('/')} src={assets.logo} alt="Logo" className='w-32 sm:w-44' />
-      <button onClick={() => navigate('/admin')} className='flex item-center rounded-full gap-2 text-sm cursor-pointer bg-primary text-white px-10 py-2.5'> {token ? 'Dashboard' : 'Login'} 
+      <button onClick={() => navigate('/admin')} className='flex items-center rounded-full gap-2 text-sm cursor-pointer bg-primary text-white px-10 py-2.5'> {token ? 'Dashboard' : 'Login'} 
         <img src={assets.arrow} alt="arrow" className='w-3' />
       </button>
     </div>
